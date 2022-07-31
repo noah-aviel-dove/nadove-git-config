189837244c95a024b450019f15ad939c005e19d9
Noah Aviel Dove <nadove@ucsc.edu>
2022-07-31 15:55:33 -0700

    Use config variables for alias inclusion (#4)

--- make_scripts.py
+++ make_scripts.py
@@ -11,10 +11,7 @@ import sys
 from typing import Iterable
 
 import arg_types
-
-root_dir = pathlib.Path(__file__).parent
-
-bash_script_prefix = '_nad_git_al_'
+from environment import bash_script_prefix
 
 cmd_head = '#' * 3
 
